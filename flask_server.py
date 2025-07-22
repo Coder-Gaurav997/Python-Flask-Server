357bb534050a1e4c6cd0b9c050d2168e9012f29f
Gaurav Pandey <mr.hacker13032010@gmail.com>
2025-07-22 18:15:05 +0530

    Update and rename server.py to flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Allow requests from Netlify
+CORS(app)  
 
 @app.route('/store', methods=['POST'])
 def store_data():
